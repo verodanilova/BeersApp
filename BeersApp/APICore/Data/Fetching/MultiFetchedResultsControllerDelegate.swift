e650fe421bd0c479cdf616898a132f897d14ad8e
Veronika Danilova <veronika.danilova@noveogroup.com>
2020-11-05 23:37:03 +0200

    Add FetchedResultsControllerDelegates

---
+++ BeersApp/APICore/Data/Fetching/MultiFetchedResultsControllerDelegate.swift
@@ -0,0 +1,83 @@
+//
+//  MultiFetchedResultsControllerDelegate.swift
+//  BeersApp
+//
+//  Created by Veronica Danilova on 07.11.2020.
+//
+
+import Foundation
+import CoreData
+import RxSwift
+import RxCocoa
+
+
+protocol MultiFetchedResultsControllerDelegateType {
+    associatedtype Item: NSManagedObject
+    var currentItem: [Item]? {get}
+    var fetchedItem: Driver<[Item]> {get}
+}
+
+final class MultiFetchedResultsControllerDelegate<Item: NSManagedObject>: NSObject,
+    NSFetchedResultsControllerDelegate, Disposable,
+    MultiFetchedResultsControllerDelegateType {
+    typealias Context = DataContext
+
+    var currentItem: [Item]?
+    let fetchedItem: Driver<[Item]>
+
+    private let request: NSFetchRequest<Item>
+
+    private let context: Context
+    private let fetchedResultsController: NSFetchedResultsController<Item>
+    private let itemSubject = ReplaySubject<[Item]>.create(bufferSize: 1)
+
+    public init(context: Context, request: NSFetchRequest<Item>) {
+        self.context = context
+        self.request = request
+
+        self.fetchedItem = itemSubject.asDriver { _ in
+                fatalError("Unexpected error occurred while converting itemSubject to Driver")
+            }
+        
+        fetchedResultsController = NSFetchedResultsController<Item>(
+            fetchRequest: request, managedObjectContext: context.managedObjectContext,
+            sectionNameKeyPath: nil, cacheName: nil)
+
+        super.init()
+
+        fetchedResultsController.delegate = self
+        performFetch()
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        try? controller.performFetch()
+        if let fetchedObject = controller.fetchedObjects as? [Item] {
+            currentItem = fetchedObject
+            sendFetchedObject(fetchedObject)
+        } else {
+            currentItem = nil
+        }
+    }
+    
+    func dispose() {
+        itemSubject.dispose()
+    }
+}
+
+private extension MultiFetchedResultsControllerDelegate {
+    func performFetch() {
+        do {
+            try fetchedResultsController.performFetch()
+            if let fetchedObject = fetchedResultsController.fetchedObjects as? [Item] {
+                currentItem = fetchedObject
+                sendFetchedObject(fetchedObject)
+            }
+        } catch {
+            print("Fetched results controller delegate failed to fetch changes. Error: \(error)")
+        }
+    }
+
+    func sendFetchedObject(_ object: [Item]) {
+        itemSubject.onNext(object)
+    }
+}
--- BeersApp/APICore/Data/PersistentModel/BeerInfo.swift
+++ BeersApp/APICore/Data/PersistentModel/BeerInfo.swift
@@ -21,3 +21,18 @@ final class BeerInfo: NSManagedObject {
     @NSManaged var bitternessIndex: Double /* IBU */
     @NSManaged var colorIndex: Double /* EBC */
 }
+
+extension BeerInfo {
+    @nonobjc class func fetchRequest() -> NSFetchRequest<BeerInfo> {
+        return NSFetchRequest(entityName: String(describing: BeerInfo.self))
+    }
+    
+    class func fetchRequest(id: Int) -> NSFetchRequest<BeerInfo> {
+        let request: NSFetchRequest<BeerInfo> = fetchRequest()
+        request.predicate = NSPredicate(format: "id == %ld", id)
+        request.sortDescriptors = [NSSortDescriptor(
+            keyPath: \BeerInfo.id, ascending: true)]
+        request.fetchLimit = 1
+        return request
+    }
+}
